5462c07d576b9345849fdd35cc55a36c995127c1
Bernardo de Oliveira Miranda Junior <bernardodeoliveiramirandajunior@192.168.1.3>
2022-01-27 07:35:28 -0400

    feat: adding snackbar before create transfer

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class BytebankApp extends StatelessWidget {
 }
 
 class TransferForm extends StatelessWidget {
-  const TransferForm({Key? key}) : super(key: key);
+  final TextEditingController _controllerAccount = TextEditingController();
+  final TextEditingController _controllerValue = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +28,12 @@ class TransferForm extends StatelessWidget {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding:
+                const EdgeInsets.only(top: 13, left: 30, right: 30, bottom: 8),
             child: TextField(
+              controller: _controllerAccount,
               decoration: InputDecoration(
+                prefixIcon: Icon(Icons.account_balance),
                 labelText: 'Número da conta',
                 hintText: '0000-0',
                 border: OutlineInputBorder(
@@ -42,9 +46,12 @@ class TransferForm extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding:
+                const EdgeInsets.only(top: 13, left: 30, right: 30, bottom: 8),
             child: TextField(
+              controller: _controllerValue,
               decoration: InputDecoration(
+                prefixIcon: Icon(Icons.monetization_on),
                 labelText: 'Valor depositado',
                 hintText: 'R\$ 00.00',
                 border: OutlineInputBorder(
@@ -62,8 +69,46 @@ class TransferForm extends StatelessWidget {
               width: 200,
               height: 40,
               child: ElevatedButton(
-                child: Text('Criar'),
-                onPressed: () {},
+                child: Text('Confirmar'),
+                onPressed: () {
+                  final String? accountNumber = _controllerAccount.text;
+                  final double? value = double.tryParse(_controllerValue.text);
+
+                  if (![accountNumber, value].contains(null)) {
+                    final createdTransfer = Transfer(accountNumber!, value!);
+                    debugPrint('$createdTransfer');
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Text(
+                                '$createdTransfer',
+                              ),
+                            ),
+                            Expanded(
+                              child: TextButton(
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentSnackBar();
+                                },
+                                child: Text(
+                                  'Fechar',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                    );
+                  }
+                },
               ),
             ),
           ),
@@ -100,7 +145,7 @@ class TransferList extends StatelessWidget {
       body: Column(
         children: [
           TransferItem(
-            Transfer('12551-2', '100'),
+            Transfer('12551-2', 100.00),
           ),
         ],
       ),
@@ -131,8 +176,11 @@ class TransferItem extends StatelessWidget {
 }
 
 class Transfer {
-  final String value;
+  final double value;
   final String account;
 
   Transfer(this.account, this.value);
+
+  @override
+  String toString() => 'Transfer(value: $value, account: $account)';
 }
